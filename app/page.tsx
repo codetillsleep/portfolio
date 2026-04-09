876410524163e20534d70866d87c02012e141f04
Saksham <sleeptillash@gmail.com>
2026-04-09 19:57:54 +0530

    bug fix

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import Contact from "./Components/Contact/Contact";
 import Footer from "./Components/Footer/Footer";
 import Hero from "./Components/Hero/Hero";
 import Projects from "./Components/Projects/Projects";
-import Skills from "./Components/Skills/Skills";
 
 export default function Home() {
   return (
@@ -13,7 +12,7 @@ export default function Home() {
       <Hero />
       <Projects />
       <About />
-      {/* <Skills /> */}
+
       <Contact />
       <Footer />
     </main>
